6d402c0979a9364b476e9d6f5c15a37c6bd70cea
skim1225 <trollpotato12@gmail.com>
2025-09-10 15:53:57 -0700

    init files for module 2 hw

---
+++ module2/cmd_ln_args/arger-kim.c
@@ -0,0 +1,16 @@
+/*
+* arger-kim.c / Command Line Arguments
+*
+* Sooji Kim / CS5600 / Northeastern University
+* Fall 2025 / Sep 10, 2025
+*
+* Program which accepts command line arguments to transform the case of some
+* input text in various ways.
+*/
+
+#include <stdio.h>   /**< Standard I/O operations. */
+
+int main(int argc, char *argv[]) {
+    char choice[] = argv[1];
+    if choice == "
+}
---
+++ module2/rng/genRand-kim.c
@@ -0,0 +1,54 @@
+/*
+* genRand-kim.c / Random Number Generation
+*
+* Sooji Kim / CS5600 / Northeastern University
+* Fall 2025 / Sep 10, 2025
+*
+* Program for performing calculation and generating random numbers in C.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+static char APPEND[] = "-a";
+
+int genRand(int min, int max) {
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    int num = atoi(argv[1]);
+    char filePath[] = argv[2];
+    int overwrite = 1;
+
+    if ( argc > 3 ) {
+        char option[] = argv[3];
+        if ( strcmp(option, APPEND) == 0 ) {
+            overwrite = 0;
+        }
+    }
+
+    // open file in write or append mode depending on cmd ln arg
+    if (overwrite) {
+        fp = fopen(filePath, "w");
+    } else {
+        fp = fopen(filePath, "a");
+    }
+
+    // check if file was opened
+    if (fp == NULL) {
+        puts("Error opening file");
+        exit(1);
+    }
+
+    // write to file the # of times specified
+    for (int i = 0; i < num; i++) {
+        fprintf(fp, "%d\n", genRand(0, 100));
+    }
+
+    // close file
+    fclose(fp);
+
+    return 0;
+}
