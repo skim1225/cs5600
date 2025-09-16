1067de6ae00854cfe69de727efd28dd09e8ec0ff
skim1225 <trollpotato12@gmail.com>
2025-09-15 23:14:26 -0700

    Fixed logic bugs for 2 cases

--- module2/cmd_ln_args/arger-kim.c
+++ module2/cmd_ln_args/arger-kim.c
@@ -9,11 +9,12 @@
 */
 
 #include <stdio.h>   /**< Standard I/O operations. */
+#include <stdlib.h>
 #include <string.h>
 
 static int OFFSET = 32;
 
-int isupper(char c) {
+int is_upper(char c) {
     if (c >= 'A' && c <= 'Z') {
         return 1;
     } else {
@@ -21,54 +22,74 @@ int isupper(char c) {
     }
 }
 
-int islower(char c) {
+int is_lower(char c) {
     if (c >= 'a' && c <= 'z') {
         return 1;
     } else {
         return 0;
     }
+}
 
 int main(int argc, char *argv[]) {
-    char choice[] = argv[1];
-    char str[] = argv[2];
-    int x = 0;
+    char *choice = argv[1];
+    char *word;
+    int x;
+    // to all uppercase
     if (strcmp(choice, "-u") == 0) {
-        while (str[x] != '\0') {
-            if (islower(str[x])) {
-                str[x] -= OFFSET;
+        for (int i = 2; i < argc; i++) {
+            word = argv[i];
+            x = 0;
+            while (word[x] != '\0') {
+                if (is_lower(word[x])) {
+                    word[x] -= OFFSET;
+                }
+                printf("%c", word[x]);
+                x++;
             }
-            x++;
+            if (i < argc - 1) {
+                printf(" ");
+            }
         }
-        printf("%s\n", str);
+        printf("\n");
+    // to all lowercase
     } else if (strcmp(choice, "-l") == 0) {
-        while (str[x] != '\0') {
-            if (isupper(str[x])) {
-                str[x] += OFFSET;
+        for (int i = 2; i < argc; i++) {
+            word = argv[i];
+            x = 0;
+            while (word[x] != '\0') {
+                if (is_upper(word[x])) {
+                    word[x] += OFFSET;
+                }
+                printf("%c", word[x]);
+                x++;
             }
-            x++;
+            if (i < argc - 1) {
+                printf(" ");
+            }
         }
-        printf("%s\n", str);
+        printf("\n");
+    // capitalize 1st letter of each word
     } else if (strcmp(choice, "-cap") == 0) {
         // check the first element
-        if (islower(str[0])) {
-            str[0] -= OFFSET;
+        if (is_lower(word[0])) {
+            word[0] -= OFFSET;
         }
         x = 1;
         // loop over all other elements and check if previous char is " "
-        while (str[x] != '\0') {
+        while (word[x] != '\0') {
             // check if 1st letter of word
-            if (strcmp(str[x-1]," ") == 0) {
-                if (islower(str[x])) {
-                    str[x] -= OFFSET;
+            if (word[x-1] == ' ') {
+                if (is_lower(word[x])) {
+                    word[x] -= OFFSET;
                 }
             } else {
-                if (isupper(str[x])) {
-                    str[x] += OFFSET;
+                if (is_upper(word[x])) {
+                    word[x] += OFFSET;
                 }
             }
             x++;
         }
-        printf("%s\n", str);
+        printf("%c", word[x]);
     } else {
         puts("Invalid choice. Valid flags are:");
         puts("-u: display text in all upper case.");
