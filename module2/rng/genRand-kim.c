141d505964db35ffa9a5349b6495b61b0a0fc504
skim1225 <trollpotato12@gmail.com>
2025-09-23 00:01:10 -0700

    Completed genRand() function

--- module2/rng/genRand-kim.c
+++ module2/rng/genRand-kim.c
@@ -9,21 +9,59 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 static char APPEND[] = "-a";
+static unsigned long random;
 
-// TODO: IMPLEMENT A PSEUDO RNG ALGORITHM
+// LCM constants - same as ANSI C
+#define A 1103515245UL
+#define C 12345UL
+#define M 2147483648UL  /* 2^31 */
+
+
+/**
+ * @brief Generate a pseudo-random integer within a specified range.
+ *
+ * This function uses a Linear Congruential Generator (LCG) to update the
+ * internal random state and produce a new pseudo-random integer. The result
+ * is then scaled to fit within the inclusive range [min, max].
+ *
+ * If @p min is greater than @p max, the two values are swapped so the range
+ * is always valid. If @p min equals @p max, the function returns that value.
+ *
+ * @param min The lower bound of the desired range (inclusive).
+ * @param max The upper bound of the desired range (inclusive).
+ *
+ * @return A pseudo-random integer in the range [min, max].
+ */
 int genRand(int min, int max) {
-    return 0;
+    if (min > max) {
+        int temp = min;
+        min = max;
+        max = temp;
+    }
+    if (min == max) {
+        return min;
+    }
+    random = ((A * random) + C) % M;
+    int range = max - min + 1;
+    double res = (double) random / (double) M;
+    res *= range;
+    res += min;
+    return (int) res;
 }
 
 int main(int argc, char *argv[]) {
+
+    random = (unsigned long) time(NULL);
+
     FILE *fp;
     int num = atoi(argv[1]);
     char filePath[] = argv[2];
     int overwrite = 1;
 
-    if ( argc > 3 ) {
+    if (argc > 3) {
         char option[] = argv[3];
         if ( strcmp(option, APPEND) == 0 ) {
             overwrite = 0;
