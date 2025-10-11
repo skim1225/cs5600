f2695e5ec0d3bfbf35d45bfd2772e032d1f38e69
skim1225 <trollpotato12@gmail.com>
2025-10-10 19:21:16 -0700

    init files

---
+++ module5/multithreading/multithreading.c
@@ -0,0 +1,13 @@
+/*
+ * multithreading.c / Multithreading in C
+ *
+ * Sooji Kim / CS5600 / Northeastern University
+ * Fall 2025 / Oct 10, 2025
+ *
+ * Program which uses multithreading to encrypt words concurrently.
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+
+// TODO: THREAD FUNC
